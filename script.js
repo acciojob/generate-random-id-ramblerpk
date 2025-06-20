2754bd62f846c9d36f567297592e31267be0077a
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-20 18:07:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function makeid(l) {
-  // write your code here
-	let result = "";
-	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-	for (let i = 0; i < l;i++ {
-		result+= char_list.charAt(Math.floor(Math.random()*char_list.length));
-	}
-	return result;
+    let result = "";
+    let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < l; i++) {
+        let randomIndex = Math.floor(Math.random() * char_list.length);
+        result += char_list.charAt(randomIndex);
+    }
+    return result;
 }
 		
 	// Do not change the code below.
