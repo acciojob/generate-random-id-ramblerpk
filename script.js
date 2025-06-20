2a344c11dd79dc91c17874a20dc7ae4db8ba3144
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-20 18:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	let result = "";
+	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	for (let i = 0; i < l;i++ {
+		result+= char_list.charAt(Math.floor(Math.random()*char_list.length));
+	}
+	return result;
 }
-
-// Do not change the code below.
+		
+	// Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
